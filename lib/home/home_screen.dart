0b579b192412a60d4a2d95ab3edf56f60bed86f2
dominhdang148 <myclone235@gmail.com>
2023-11-13 13:43:50 +0700

    UPdate

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -9,78 +9,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  double _lon = 0;
-  double _lat = 0;
-  double _tilt = 0;
-  int _panoId = 0;
-  List<Image> panoList = [
-    Image.asset('assets/panorama_0.jpg'),
-    Image.asset('assets/panorama_1.jpg'),
-    Image.asset('assets/panorama_2.jpg'),
-  ];
-
-  void onViewChanged(longitude, latitude, tilt) {
-    setState(() {
-      _lon = longitude;
-      _lat = latitude;
-      _tilt = tilt;
-    });
-  }
-
-  Widget hotspotButton(
-      {String? text, IconData? icon, VoidCallback? onPresseed}) {
-    return const Column();
-  }
-
   @override
   Widget build(BuildContext context) {
-    Widget panorama;
-    switch (_panoId % panoList.length) {
-      case 0:
-        panorama = PanoramaViewer(
-          animSpeed: .1,
-          sensorControl: SensorControl.orientation,
-          onViewChanged: onViewChanged,
-          onTap: (longitude, latitude, tilt) =>
-              debugPrint('onTap: $longitude, $latitude, $tilt'),
-          onLongPressStart: (longitude, latitude, tilt) =>
-              debugPrint('onLongPressStart: $longitude, $latitude, $tilt'),
-          onLongPressMoveUpdate: (longitude, latitude, tilt) =>
-              debugPrint('onLongPressMoveUpdate: $longitude, $latitude, $tilt'),
-          onLongPressEnd: (longitude, latitude, tilt) =>
-              debugPrint('onLongPressEnd: $longitude, $latitude, $tilt'),
-          hotspots: [
-            Hotspot(
-              latitude: -15.0,
-              longitude: -129.0,
-              width: 90,
-              height: 75,
-              widget: hotspotButton(
-                text: "Next scene",
-                icon: Icons.open_in_browser,
-                onPresseed: () => setState(() => _panoId++),
-              ),
-            ),
-          ],
-        );
-        break;
-      case 1:
-        break;
-      case 2:
-        break;
-      default:
-    }
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Panorama 360 view Flutter"),
+        title: const Text("Panorama view test"),
       ),
-      body: Stack(
-        children: [
-          Text(
-            '${_lon.toStringAsFixed(3)}, ${_lat.toStringAsFixed(3)}. ${_tilt.toStringAsFixed(3)}',
-          )
-        ],
-      ),
+      body: Center(
+          child: PanoramaViewer(
+        animSpeed: 1.0,
+        child: Image.asset('assets/panorama_0.jpg'),
+      )),
     );
   }
 }
